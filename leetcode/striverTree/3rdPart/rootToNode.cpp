8bed2dc6623e350c882ebc469823083242e76640
Ryaaan4321 <aryan056400@gmail.com>
2024-04-13 14:13:23 +0530

    13/04/2024

--- leetcode/striverTree/3rdPart/rootToNode.cpp
+++ leetcode/striverTree/3rdPart/rootToNode.cpp
@@ -20,8 +20,26 @@ struct TreeNode
 class Solution
 {
 public:
-    vector<string> allRootToLeaf(TreeNode *root)
+    bool search(TreeNode *root, TreeNode *node, vector<int> &ans)
     {
-        
+        if (root == NULL)
+        {
+            return 0;
+        }
+        if (root == node)
+        {
+            return 1;
+        }
+        return search(root->left, node, ans) || search(root->right, node, ans);
+    }
+    vector<int> allRootToLeaf(TreeNode *root, TreeNode *p)
+    {
+        vector<int> ans;
+        if (root == NULL)
+        {
+            return ans;
+        }
+        search(root, p, ans);
+        return ans;
     }
 };
