8de8c0d32c7a6790576f8b10f47bf81995127f7e
Aryan Bhashkar <aryan056400@gmail.com>
2024-06-09 09:46:55 +0530

    09/06/2024

--- leetcode/Contest/virtualContest/contest320/2476.cpp
+++ leetcode/Contest/virtualContest/contest320/2476.cpp
@@ -15,40 +15,41 @@ struct TreeNode
         left = right = nullptr;
     }
 };
-class Solution{
-    public:
-    vector<int>in;
-    void inorder(TreeNode* node){
-        if(node==NULL){return;}
+class Solution {
+public:
+    vector<int> in;
+    void inorder(TreeNode* node) {
+        if (node == NULL)
+            return;
         inorder(node->left);
         in.push_back(node->val);
         inorder(node->right);
     }
-    vector<int> search(int _lo,int _hi,int x){
-        int _mn=-1;
-        int _mx=-1;
-        while(_lo<=_hi){
-            int mid=_lo+(_hi-_lo)/2;
-            if(in[mid]==x){
-                return {in[mid],in[mid]};
-            }
-            if(in[mid]<x){
-                _lo=mid+1;
-                _mn=in[mid];
-            }else{
-                _hi=mid-1;
-                _mx=in[mid];
+    vector<int> search(int x, int lo_, int hi_) {
+        int mini = -1, maxi = -1;
+        while (lo_ <= hi_) {
+            int mid = lo_ + (hi_ - lo_) / 2;
+
+            if (in[mid] == x)
+                return {in[mid], in[mid]};
+
+            if (in[mid] < x) {
+                lo_ = mid + 1;
+                mini = in[mid];
+            } else {
+                hi_ = mid - 1;
+                maxi = in[mid];
             }
         }
-        return {_mn,_mx};
+
+        return {mini, maxi};
     }
     vector<vector<int>> closestNodes(TreeNode* root, vector<int>& q) {
         inorder(root);
-        vector<vector<int>>ans;
-        for(int i=0;i<q.size();i++){
-             ans.push_back(search(0,q.size()-1,q[i]));
+        vector<vector<int>> ans;
+        for (int i = 0; i < q.size(); i++) {
+            ans.push_back(search(q[i], 0, in.size() - 1));
         }
         return ans;
     }
-
 };
