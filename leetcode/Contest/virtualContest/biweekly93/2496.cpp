7cb1693741c931e6b99663610123925ad3197a04
Aryan Bhashkar <aryan056400@gmail.com>
2024-06-25 13:26:55 +0530

    25/06/2024

--- leetcode/Contest/virtualContest/biweekly93/2496.cpp
+++ leetcode/Contest/virtualContest/biweekly93/2496.cpp
@@ -1,6 +1,22 @@
-#include<bits/stdc++.h>
-#include<algorithm>
-#include<math.h>
+// class Solution {
+//     public int maximumValue(String[] strs) {
+//         int ans=0;
+//         for(String s:strs){
+//             ans=Math.max(ans, x(s));
+//         }
+//         return ans;
 
-using namespace std;
-
+        
+//     }
+//     private int x(String s){
+//         for(int i=0;i<s.length();i++){
+//             if(s.charAt(i)>='a' && s.charAt(i)<='z'){
+//                 return s.length();
+//             }
+        
+//         }
+//         return Integer.parseInt(s);
+//     }
+    
+// }
+//
