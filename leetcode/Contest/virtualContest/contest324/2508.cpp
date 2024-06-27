da8e09d789a2e2c6b687e836ae65ef4ca91abbbb
Aryan Bhashkar <aryan056400@gmail.com>
2024-06-27 14:32:27 +0530

    27/02/2024

---
+++ leetcode/Contest/virtualContest/contest324/2508.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+#include<math.h>
+
+using namespace std;
+
+
+class Solution {
+public:
+    bool isPossible(int n, vector<vector<int>>& edges) {
+        
+    }
+};
