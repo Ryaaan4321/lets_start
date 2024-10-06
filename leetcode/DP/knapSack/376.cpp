c0f2bad50214f214da702510baa6437c8d538291
minioncodes <aryan@digipants.com>
2024-10-06 15:12:19 +0530

    DP

--- leetcode/DP/knapSack/376.cpp
+++ leetcode/DP/knapSack/376.cpp
@@ -27,7 +27,27 @@ public:
         return cnt;
         /* bodmoshi ni karega madarchood*/
     }
-    int wiggleMaxLength(vector<int>& a) {
-        
+};
+
+class Solution {
+public:
+    int fucc(int idx,int pidx,int dif,int n,vector<int>&a,vector<vector<vector<int>>>&dp){
+        if(idx==n){
+            return 0;
+        }
+        if(dp[idx][pidx+1][dif]!=-1){
+            return dp[idx][pidx+1][dif];
+        }
+        int take=INT_MIN;
+        if((pidx==-1 || (dif==0 && a[pidx]>a[idx])) || (pidx==-1 || (dif==1 && a[pidx]<a[idx]))){
+            take=1+fucc(idx+1,idx,!dif,n,a,dp);
+        }
+        int ntake=0+fucc(idx+1,pidx,dif,n,a,dp);
+        return dp[idx][pidx+1][dif]=max(take,ntake);
+    }
+    int wiggleMaxLength(vector<int>&a) {
+        int n=a.size();
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(n+2,vector<int>(3,-1)));
+        return max(fucc(0,-1,0,n,a,dp),fucc(0,-1,1,n,a,dp));
     }
 };
