9aa9fea52cb7dc904b74ab02e0d3f78d8a111259
Ryaaan4321 <aryan056400@gmail.com>
2024-11-24 09:20:48 +0530

    24.11.24

---
+++ leetcode/Contest/biweekly144/3360.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <math.h>
+#include <algorithm>
+
+class Solution
+{
+public:
+    int check(int n)
+    {
+        int c = 0;
+        int num = 10;
+        n = n - num;
+        if (n < 0)
+        {
+            return c;
+        }
+        while (n >= 0)
+        {
+            n = n - (num - 1);
+            num = num - 1;
+            c++;
+        }
+        return c;
+    }
+    bool canAliceWin(int n)
+    {
+        if (n < 10)
+        {
+            return 0;
+        }
+        return check(n) & 1 ? 1 : 0;
+    }
+};
