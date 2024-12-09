c553b5cff41fd37241eec45b722af5f504c5b11c
Ryaaan4321 <aryan056400@gmail.com>
2024-12-09 22:44:40 +0530

    09.12.24

--- leetcode/Contest/contest398/3152.cpp
+++ leetcode/Contest/contest398/3152.cpp
@@ -3,38 +3,23 @@
 #include <math.h>
 
 using namespace std;
-/*you gotta be almost insane to your craft - mc gregor*/
 class Solution {
 public:
-    bool isvalid(int s, int e, vector<int>& a) {
-        int n = a.size();
-        for (int i = s; i < e; i++) {
-            if (i + 1 <= n && a[i] & 1 && a[i + 1] & 1) {
-                return false;
-            }
-            if (i + 1 <= n && a[i] % 2 == 0 && a[i + 1] % 2 == 0) {
-                return false;
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& grid) {
+        int n = nums.size();
+        vector<int> prefix(n, 0);
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1];
+            if ((nums[i] & 1) == (nums[i - 1] & 1)) {
+                prefix[i]++;
             }
         }
-        return true;
-    }
-    vector<bool> isArraySpecial(vector<int>& a, vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<bool> ans(n);
-        int k = a.size();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int fr = grid[i][0];
-                int to = grid[i][1];
-                if (isvalid(fr, to, a)) {
-                    ans[i] = true;
-                } else if (!isvalid(fr, to, a)) {
-                    ans[i] = false;
-                }
-            }
+        vector<bool> ans;
+        for (auto& q : grid) {
+            int left = q[0], right = q[1];
+            int specialCount = prefix[right] - (left > 0 ? prefix[left] : 0);
+            ans.push_back(specialCount == 0);
         }
         return ans;
     }
 };
-/*tle in the second last test case*/
