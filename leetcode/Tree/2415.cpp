bf0802f1ee7470a196cd6939b27f719dff445583
Ryaaan4321 <aryan056400@gmail.com>
2024-12-20 09:09:18 +0530

    Refactor Solution class and improve reverseOddLevels function for better readability and efficiency

--- leetcode/Tree/2415.cpp
+++ leetcode/Tree/2415.cpp
@@ -15,20 +15,54 @@ struct TreeNode
         left = right = NULL;
     }
 };
-class Solution {
+class Solution
+{
 public:
-    void fuc(TreeNode* l, TreeNode* r, int lev) {
+    void fuc(TreeNode *l, TreeNode *r, int lev)
+    {
         if (!l || !r)
             return;
-        if (lev&1) {
+        if (lev & 1)
+        {
             swap(l->val, r->val);
         }
         lev++;
         fuc(l->left, r->right, lev);
         fuc(l->right, r->left, lev);
     }
-    TreeNode* reverseOddLevels(TreeNode* root) {
-        fuc(root->left, root->right, 1);
+    TreeNode *reverseOddLevels(TreeNode *root)
+    {
+        queue<TreeNode *> q;
+        q.push(root);
+        bool f = 0;
+        while (!q.empty())
+        {
+            int sz = q.size();
+            vector<TreeNode *> arr(sz);
+            for (int i = 0; i < sz; i++)
+            {
+                auto node = q.front();
+                q.pop();
+                if (node->left)
+                {
+                    q.push(node->left);
+                }
+                if (node->right)
+                {
+                    q.push(node->right);
+                }
+                if (f)
+                {
+                    arr[i] = node;
+                    if (i >= sz / 2)
+                    {
+                        swap(arr[i]->val, arr[sz - 1 - i]->val);
+                    }
+                }
+            }
+            f = !f;
+        }
         return root;
     }
-};;
+};
+;
