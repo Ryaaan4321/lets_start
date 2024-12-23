a45fcb94b6c3a299e611abbe3ffa3296f9f395f9
Ryaaan4321 <aryan056400@gmail.com>
2024-12-23 09:13:31 +0530

    Add minimumOperations function to Solution class for tree node processing

---
+++ leetcode/Tree/2471.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+#include<math.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+    int minimumOperations(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        int cnt = 0;
+        while (!q.empty()) {
+            int qz = q.size();
+            vector<int> idx(qz, 0);
+            vector<int> arr(qz, 0);
+            iota(idx.begin(), idx.end(), 0);
+            for (int i = 0; i < qz; i++) {
+                auto node = q.front();
+                q.pop();
+                arr[i] = node->val;
+                if (node->left)
+                    q.push(node->left);
+                if (node->right)
+                    q.push(node->right);
+            }
+            sort(idx.begin(), idx.end(), [&](int i, int j){
+                return arr[i]<arr[j];
+            });
+            for(int i=0;i<qz;){
+                int j = idx[i];
+                if (j != i) {
+                    cnt++;
+                    swap(idx[i], idx[j]);
+                } else {
+                    i++;
+                }
+            }
+        }
+        return cnt;
+    }
+};
