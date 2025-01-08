0e0791cbaed40bb2a971d52248b0e9dbae6ce2fd
Ryaaan4321 <aryan056400@gmail.com>
2025-01-08 21:44:12 +0530

    Implement minOperations function in Solution class and add getSubarrayBeauty function for subarray beauty calculation

--- leetcode/Contest/virtualContest1/contest342/4.cpp
+++ leetcode/Contest/virtualContest1/contest342/4.cpp
@@ -7,10 +7,23 @@ using namespace std;
 
 class Solution {
 public:
-    int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
-        int time=(arrivalTime+delayedTime);
-        int final=time%24;
-        return final;
-        
+    int minOperations(vector<int>& nums) {
+        int n=nums.size();
+        int c=count(begin(nums),end(nums),1);
+        if(c!=0){
+           return n-c;
+        }
+        int  res=1e7;
+        for(int i=0;i<n;i++){
+            int g=nums[i];
+            for(int j=i+1;j<n;j++){
+                g=__gcd(g,nums[j]);
+                if(g==1){
+                    res=min(res,j-i+(n-1));
+                    break;
+                }
+            }
+        }
+        return res==1e7?-1:res;
     }
 };
