711bbaa798773bef3373e54b65124d1f08731aa6
Ryaaan4321 <aryan056400@gmail.com>
2025-01-13 22:06:21 +0530

    added

--- leetcode/Contest/contest432/3418.cpp
+++ leetcode/Contest/contest432/3418.cpp
@@ -3,10 +3,7 @@ using namespace std;
 
 class Solution {
 public:
-    long long dp[501][501][3];
-    bool vis[501][501][3];
-
-    long long fucc(int i, int j, int k, vector<vector<int>>& a) {
+    long long fucc(int i, int j, int k, vector<vector<int>>& a ,vector<vector<vector<long long>>>dp,vector<vector<vector<bool>>>vis) {
         int n = a.size();
         int m = a[0].size();
         if (i >= n || j >= m) return INT_MIN;
@@ -18,20 +15,22 @@ public:
             return dp[i][j][k];
         }
         long long t1 = INT_MIN, t2 = INT_MIN;
-        t1 = a[i][j] + fucc(i + 1, j, k, a);
+        t1 = a[i][j] + fucc(i + 1, j, k, a,dp,vis);
         if (a[i][j] < 0 && k > 0) {
-            t1 = max(t1, fucc(i + 1, j, k - 1, a));
+            t1 = max(t1, fucc(i + 1, j, k - 1, a,dp,vis));
         }
-        t2 = a[i][j] + fucc(i, j + 1, k, a);
+        t2 = a[i][j] + fucc(i, j + 1, k, a,dp,vis);
         if (a[i][j] < 0 && k > 0) {
-            t2 = max(t2, fucc(i, j + 1, k - 1, a));
+            t2 = max(t2, fucc(i, j + 1, k - 1, a,dp,vis));
         }
         vis[i][j][k] = 1;
         return dp[i][j][k] = max(t1, t2);
     }
     int maximumAmount(vector<vector<int>>& coins) {
-        memset(dp, -1, sizeof(dp));
-        memset(vis, 0, sizeof(vis));
-        return fucc(0, 0, 2, coins);
+        int n=coins.size();
+        int m=coins[0].size();
+        vector<vector<vector<long long>>>dp(n,vector<vector<long long>>(m,vector<long long>(3,-1)));
+        vector<vector<vector<bool>>>vis(n,vector<vector<bool>>(m,vector<bool>(3,0)));
+        return fucc(0,0,3,coins,dp,vis);
     }
 };
