4d45e4a0f79550259eb72683bf6ed2536fec0b41
Ryaaan4321 <aryan056400@gmail.com>
2025-01-17 22:20:21 +0530

    Implement findThePrefixCommonArray and countOperationsToEmptyArray functions in Solution class

--- leetcode/Contest/virtualContest1/biweekly103/2657.cpp
+++ leetcode/Contest/virtualContest1/biweekly103/2657.cpp
@@ -7,6 +7,17 @@ using namespace std;
 class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        
+        int n=A.size();
+        vector<int>freq(n+1,0);
+        vector<int>ans(n);
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            freq[A[i]]++;
+            if(freq[A[i]]==2)cnt++;
+            freq[B[i]]++;
+            if(freq[B[i]]==2)cnt++;
+            ans[i]=cnt;
+        }
+        return ans;
     }
 };
--- leetcode/Contest/virtualContest1/biweekly103/2659.cpp
+++ leetcode/Contest/virtualContest1/biweekly103/2659.cpp
@@ -8,6 +8,18 @@ using namespace std;
 class Solution {
 public:
     long long countOperationsToEmptyArray(vector<int>& nums) {
-        
+        map<int, int> freq;
+        int n = nums.size();
+        long long res = n;
+        for (int i = 0; i < n; i++) {
+            freq[nums[i]] = i;
+        }
+        sort(begin(nums),end(nums));
+        for (int i = 1; i < n; i++) {
+            if (freq[nums[i]] < freq[nums[i - 1]]) {
+                res += n - i;
+            }
+        }
+        return res;
     }
 };
