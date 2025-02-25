e06afacdd182429445ecac9f2b69d623ffdef603
Ryaaan4321 <aryan056400@gmail.com>
2025-02-26 00:07:04 +0530

    learned to solve this problem using the dsu

--- leetcode/Contest/virtualContest1/contest345/2685.cpp
+++ leetcode/Contest/virtualContest1/contest345/2685.cpp
@@ -17,6 +17,27 @@ public:
             }
         }
     }
+    bool fuccc(int node, map<int,vector<int>>graph,vector<bool>&vis){
+        vis[node]=true;
+        int nodecount=0;
+        int edgecount=0;
+        stack<int>st;st.push(node);
+        while (!st.empty())
+        {
+            auto elm=st.top();
+            st.pop();
+            nodecount++;
+            edgecount+=graph[elm].size();
+            for(auto &nei:graph[elm]){
+                if(!vis[nei]){
+                    st.push(nei);
+                    vis[nei]=true;
+                }
+            }
+        }
+        return (nodecount*(nodecount-1)==edgecount);
+        
+    }
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
         map<int, vector<int>> graph;
         vector<bool> vis(n);
@@ -37,4 +58,36 @@ public:
         }
         return cnt;
     }
+    vector<int>parent,edegcount,nodecount;
+    int findparent(int node){
+        if(parent[node]==node){
+            return node;
+        }
+        return findparent(parent[node]);
+    };
+    void union_nodes(int a,int b){
+        int pa=findparent(a);
+        int pb=findparent(b);
+        edegcount[pa]+=1;
+        if(pa!=pb){
+            parent[pb]=pa;
+            edegcount[pa]+=edegcount[pb];
+            nodecount[pa]+=nodecount[pb];
+        }
+    }
+    int countCompleteComponennt(int n,vector<vector<int>>&edges){
+       int ans=0;
+       parent.assign(n,-1);
+       edegcount.assign(n,0);
+       nodecount.assign(n,1);
+       for(auto edge:edges){
+        union_nodes(edge[0],edge[1]);
+       }
+       int ans=0;
+       for(int i=0;i<n;i++){
+        if(nodecount[i] && parent[i]==-1 && (nodecount[i]-1)/2==edegcount[i])ans++;
+       }
+       return ans;
+    }
+
 };
